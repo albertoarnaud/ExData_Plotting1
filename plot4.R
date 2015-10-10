3f9391dba9afb924fae5a0a22da63575ce628a1e
Alberto <alberto.arnaud@gmail.com>
2015-10-11 00:48:45 +0200

    Update plot4.R

--- plot4.R
+++ plot4.R
@@ -1,15 +1,20 @@
 plot4 <- function() {
       
+      #loads the dataset and casts date and time values
       file <- read.table("household_power_consumption.txt", header=TRUE, na.strings=c("?", ""), sep=";")
       file$Date <- as.Date(file$Date, format = "%d/%m/%Y")
       file$timetemp <- paste(file$Date, file$Time)
       file$Time <- strptime(file$timetemp, format = "%Y-%m-%d %H:%M:%S")
       
+      #subsets the dataset on the 2 in scope dates 
       data <- subset(file, file$Date == "2007-02-01" | file$Date == "2007-02-02")
       
+      #opens a png device
       png("plot4.png", width = 480, height = 480, units = "px")
      
+      #defines a 2x2 matrix containing 2 graphs per row and 2 per column
       par(mfrow = c(2,2))
+      #plots the 4 graph
       with(data, {
       plot(Time, Global_active_power, type = "l", ylab="Global Active Power", xlab="")
       
@@ -24,5 +29,6 @@ plot4 <- function() {
       plot(Time, Global_reactive_power, type = "l", ylab="Global_reactive_power", xlab="datetime")
       })
       
+      #closes the device
       dev.off()
 }
