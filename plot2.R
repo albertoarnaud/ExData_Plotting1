3b44ba49b4c6b0432c48cee34b03ad803335eb44
Alberto <alberto.arnaud@gmail.com>
2015-10-11 00:44:31 +0200

    Update plot2.R

--- plot2.R
+++ plot2.R
@@ -1,12 +1,15 @@
 plot2 <- function() {
       
+      #loads the dataset and casts date and time values
       file <- read.table("household_power_consumption.txt", header=TRUE, na.strings=c("?", ""), sep=";")
       file$Date <- as.Date(file$Date, format = "%d/%m/%Y")
       file$timetemp <- paste(file$Date, file$Time)
       file$Time <- strptime(file$timetemp, format = "%Y-%m-%d %H:%M:%S")
       
+      #subsets the dataset on the 2 in scope dates
       data <- subset(file, file$Date == "2007-02-01" | file$Date == "2007-02-02")
       
+      # opens a png device, plots the graph and closes the device
       png("plot2.png", width = 480, height = 480, units = "px")
       plot(data$Time, data$Global_active_power, type = "l", ylab = "Global Active Power (kilowatts)", xlab="")
       dev.off()
